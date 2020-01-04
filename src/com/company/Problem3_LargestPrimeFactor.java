494bca43afa772dc721143ebd0050f3f01c55c5b
Zdravko <Zdravko.kardzhaliyski@gmail.com>
2020-01-04 23:21:37 +0200

    Fixed Some Variable Names

--- src/com/company/Problem3_LargestPrimeFactor.java
+++ src/com/company/Problem3_LargestPrimeFactor.java
@@ -1,48 +1,44 @@
 package com.company;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Problem3_LargestPrimeFactor {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>();
+        List<Integer> primes = new ArrayList<>();
         var givenNum = 600851475143.0;
         var maxNum = (int) Math.sqrt(givenNum);
         for (int i = 3; i <= maxNum; i += 2) {
-            nums.add(i);
+            primes.add(i);
         }
 
         var currPosition = 0;
-        var currNum = nums.get(currPosition);
-        while (currPosition < nums.size()) {
-            nums = test(nums, currNum);
+        var currNum = primes.get(currPosition);
+
+        while (currPosition < primes.size()) {
+            primes = removeNotPrimeNumDividedBy(primes, currNum);
             currPosition++;
 
-            if (currPosition < nums.size()) {
-                currNum = nums.get(currPosition);
+            if (currPosition < primes.size()) {
+                currNum = primes.get(currPosition);
             }
 
+            if(currNum > 5000) { //Added to reduce the time running
+                break;
+            }
 //            if (currPosition % 100 == 0)
 //                System.out.println("Prime Counter: " + nums.size() + " Current Num: " + currNum + " Last Num: " + nums.get(nums.size() -1));
         }
 
-        System.out.println("Primes found.");
-        System.out.println("Loading prime factors ...");
-
-        for (int i = nums.size() - 1; i >= 0; i--) {
-            if (givenNum % nums.get(i) == 0) {
-                System.out.println(nums.get(i));
+        for (int i = primes.size() - 1; i >= 0; i--) {
+            if (givenNum % primes.get(i) == 0) {
+                System.out.println(primes.get(i));
+                break;
             }
         }
-
-        System.out.println();
-
     }
 
-    private static List<Integer> test(List<Integer> list, int num) {
+    private static List<Integer> removeNotPrimeNumDividedBy(List<Integer> list, int num) {
         return list
                 .stream()
                 .filter(n -> {
